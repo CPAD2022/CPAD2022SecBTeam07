2f7cd3669fba110bf88e76ffd293adb3d409454d
sheldon12rodrig <s.rodrigues@sap.com>
2022-12-02 12:54:57 +0530

    Added profile page

---
+++ lib/index.dart
@@ -0,0 +1,15 @@
+// Export pages
+export 'login/login_widget.dart' show LoginWidget;
+export 'create_account/create_account_widget.dart' show CreateAccountWidget;
+export 'my_trips/my_trips_widget.dart' show MyTripsWidget;
+export 'chat_main/chat_main_widget.dart' show ChatMainWidget;
+export 'chat_details/chat_details_widget.dart' show ChatDetailsWidget;
+export 'profile_page/profile_page_widget.dart' show ProfilePageWidget;
+export 'edit_profile/edit_profile_widget.dart' show EditProfileWidget;
+export 'change_password/change_password_widget.dart' show ChangePasswordWidget;
+export 'swipe/swipe_widget.dart' show SwipeWidget;
+export 'wallet/wallet_widget.dart' show WalletWidget;
+export 'view_customer/view_customer_widget.dart' show ViewCustomerWidget;
+export 'requesters/requesters_widget.dart' show RequestersWidget;
+export 'addjob/addjob_widget.dart' show AddjobWidget;
+export 'job_details/job_details_widget.dart' show JobDetailsWidget;
